ae149a90d4dd48031087e5de321698e13bfbe8ce
BenGeordie <school@bengeordie@gmail.com>
2021-11-12 22:33:21 -0600

    Refactored rich text model! Doesn't use semidirect product yet.

--- demos/rich-text-yata/src/rich_text_sdp.ts
+++ demos/rich-text-yata/src/rich_text_sdp.ts
@@ -2,44 +2,25 @@ import * as crdts from "@collabs/collabs";
 
 interface RichTextCharacterEvents extends crdts.CrdtEventsRecord {}
 
-// Let's first try to define m1, m2, and |>
-// Vaguely, m1 is a formatting action, and m2 is an inserting action.
-// BOB     "a" -> "A"  -> "AB"
-// ALICE   "a" -> "ab" -> "AB"
-// So let's define m2 to be: insert string, and copy all formatting attributes from left unless the attribute is known to be different at input time.
-// Define m1 to be: change the formatting attributes of a character accordingly.
-
 class RichTextCharacter extends crdts.CObject<RichTextCharacterEvents> {
   // This is the structure of each object in the array.
   public character: string; // Character being represented
   public attributes: crdts.LwwCMap<string, any>; // Current attributes CRDT
-  public attributesDifferentFromLeftChar: string[]; // List of attribute names that are different from origin at create time
-  public attributesAtCreation: Record<string, any>; 
-  public attributesInitialized: boolean;
-
-  // TODO: What if I take in a reference to the CRDT of the origin?
-  // TODO: What if I just carry over formatting from the left unless different?
-
+  
   constructor(
     initToken: crdts.CrdtInitToken,
     character: string,
-    attributesDifferentFromLeftChar: string[],
-    attributesAtCreation: Record<string, any>
   ) {
     super(initToken);
     this.character = character;
-    this.attributesDifferentFromLeftChar = attributesDifferentFromLeftChar;
-    this.attributesAtCreation = attributesAtCreation;
     this.attributes = this.addChild("attributes", crdts.Pre(crdts.LwwCMap)());
-    this.attributesInitialized = false;
   }
 
-  initializeAttributesLocally(attributes: Record<string, any>) {
+  inheriteLeftAttributesLocally(attributes: crdts.LwwCMap<string, any>) {
     this.runtime.runLocally(() => {
-      attributes.entries.forEach((key: string, value: any) => {
-        this.attributes.set(key, value);
-      });
-      this.attributesInitialized = true;
+      for (const kv of attributes.entries()) {
+        this.attributes.set(kv[0], kv[1]);
+      }
     });
   }
 }
@@ -48,8 +29,6 @@ interface RichTextEvents extends crdts.CrdtEventsRecord {}
 
 type RichTextArrayInsertArgs = [
   character: string,
-  attributesDifferentFromLeftChar: string[],
-  attributesAtCreation: Record<string, any>
 ];
 
 class RichText extends crdts.CObject<RichTextEvents> {
@@ -57,6 +36,19 @@ class RichText extends crdts.CObject<RichTextEvents> {
     RichTextCharacter,
     RichTextArrayInsertArgs
   >;
+  
+  private rtCharArrayInsertHandler(e: crdts.CListInsertEvent) {
+    for (let i = e.startIndex; i < e.startIndex + e.count; i++) {
+      const leftCharAttributes: crdts.LwwCMap<string, any> = this.rtCharArray.get(i - 1).attributes;
+      this.rtCharArray.get(i).inheriteLeftAttributesLocally(leftCharAttributes);
+    }
+  };
+
+  private insertTextAndInheritAttributes(startIndex: number, text: string) {
+    text.split("").forEach((c, i) => {
+      this.rtCharArray.insert(startIndex + i + 1, c);
+    });
+  }
 
   constructor(initToken: crdts.CrdtInitToken) {
     super(initToken);
@@ -66,28 +58,38 @@ class RichText extends crdts.CObject<RichTextEvents> {
         (
           valueInitToken: crdts.CrdtInitToken,
           character: string,
-          attributesDifferentFromLeftChar: string[],
-          attributesAtCreation: Record<string, any>
         ) => {
           return new RichTextCharacter(
             valueInitToken,
-            character,
-            attributesDifferentFromLeftChar,
-            attributesAtCreation
+            character
           );
         },
         [
-          ["start", [], {}],
-          ["end", [], {}],
+          ["start"],
+          ["end"],
         ]
       )
     );
-    this.rtCharArray.on("Insert", ({}) => {});
+    this.rtCharArray.on("Insert", this.rtCharArrayInsertHandler); // Ini apa ya?
   }
-  
 
-  rtCharArrayInsertHandler(e: crdts.CListInsertEvent) {
-    this.rtCharArray.
-    e.
-  };
+  public formatText(startIndex: number, count: number, attributes: Record<string, any>) {
+    for (let i = startIndex + 1; i < startIndex + 1 + count; i++) {
+      const rtCharCrdtAttributes = this.rtCharArray.get(i).attributes;
+      Object.entries(attributes).forEach((kv) => {
+        rtCharCrdtAttributes.set(kv[0], kv[1]);
+      });
+    }
+  }
+
+  public insertTextWithAttributes(startIndex: number, text: string, attributes: Record<string, any>) {
+    // TODO: Get unique attributes. (Should I handle this logic here or in the cod ethat integrates it with quill?)
+    const leftAttributes = this.rtCharArray.get(startIndex).attributes;
+    const uniqueAttributes = Object.fromEntries(Object.entries(attributes).filter((kv) => {
+      kv[1] !== leftAttributes.get(kv[0]);
+    }));
+
+    this.insertTextAndInheritAttributes(startIndex, text);
+    this.formatText(startIndex, text.length, uniqueAttributes);
+  }
 }
